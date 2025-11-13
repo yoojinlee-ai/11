a389ac4be24e19342264bf67bf1919fac2d7a401
yoojinlee-ai <yoojinlee@sookmyung.ac.kr>
2025-11-13 14:07:50 +0900

    11-3(2)

--- main.c
+++ main.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
 
-void swap(int x, int y) {
+void swap(int *x, int *y) {
     int temp;
-    temp = x;
-    x = y;
-    y = temp;
+    temp = *x;
+    *x = *y;
+    *y = temp;
 }
 
 int main(void) {
     int a = 3;
     int b = 5;
-    swap(a, b);
+    swap(&a, &b);
     printf("a:%i, b:%i\n", a, b);
     return 0;
 }
