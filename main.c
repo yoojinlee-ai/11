09b29c8aa57670ba1176d754c993e3fd42b5925d
yoojinlee-ai <yoojinlee@sookmyung.ac.kr>
2025-11-13 14:12:42 +0900

    2'

--- main.c
+++ main.c
@@ -1,29 +1,35 @@
 #include <stdio.h>
+#include <string.h>
 
-#define TYPE_INT 0
-#define TYPE_CHAR 1
-#define TYPE_DOUBLE 2
-#define TYPE_FLOAT 3
-#define TYPE_STRING 4
+typedef struct {
+    char name[20];
+    int energy;
+} Player;
 
-char typeNames[5][10] = {
-    "INT",
-    "CHAR",
-    "DOUBLE",
-    "FLOAT",
-    "STRING"
-};
-
-const char* getName(int type)
+void generatePlayers(Player players[], int n, int initEnergy)
 {
-    return typeNames[type];
+    int i;
+    for (i = 0; i < n; i++) {
+        printf("플레이어 %d 이름 입력: ", i + 1);
+        scanf("%s", players[i].name);
+        players[i].energy = initEnergy;
+    }
 }
 
 int main(void)
 {
+    int n;
+    printf("플레이어 수 입력: ");
+    scanf("%d", &n);
+    getchar();
+
+    Player players[10];
+    generatePlayers(players, n, 100);
+
     int i;
-    for (i = 0; i < 5; i++)
-        printf("%s\n", getName(i));
+    for (i = 0; i < n; i++)
+        printf("이름: %s, 에너지: %d\n", players[i].name, players[i].energy);
+
     return 0;
 }
 
